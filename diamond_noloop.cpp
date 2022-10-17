5c600349bf766b7462c7b8c774c083cdea6e0c0f
jeffreyL <jeff12480@gmail.com>
2022-10-17 12:04:31 +0800

    Add files via upload

---
+++ diamond_noloop.cpp
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2021 Xilinx, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "diamond.h"
+
+void diamond(data_t vecIn[N], data_t vecOut[N])
+{
+  data_t c1[N], c2[N], c3[N], c4[N];
+//#pragma HLS dataflow
+#pragma HLS dataflow
+  Loop0:
+    for (int i = 0; i < N; i++)
+    {
+	#pragma HLS pipeline II=1 rewind
+	#pragma HLS unroll factor = 2
+
+	  funcA(vecIn, c1[i], c2[i]);
+	  funcB(c1[i], c3[i]);
+	  funcC(c2[i], c4[i]);
+	  funcD(c3[i], c4[i], vecOut);
+    }
+}
+
+void funcA(data_t in, data_t out1, data_t out2)
+{
+	/*
+#pragma HLS INTERFACE mode=ap_fifo port=*in
+#pragma HLS INTERFACE mode=ap_fifo port=*out1
+#pragma HLS INTERFACE mode=ap_fifo port=*out2
+
+
+
+#pragma HLS pipeline II=1 rewind //rewind shall only appear at the top level?
+#pragma HLS unroll factor = 2
+*/
+//try style=stp flp frp
+    data_t t = in * 3;
+    out1 = t;
+    out2 = t;
+
+}
+
+void funcB(data_t in, data_t out )
+{
+	/*
+#pragma HLS INTERFACE mode=ap_fifo port=*in
+#pragma HLS INTERFACE mode=ap_fifo port=*out
+
+
+#pragma HLS pipeline II=1 rewind
+#pragma HLS unroll factor = 2
+*/
+    out = in + 25;
+
+}
+
+void funcC(data_t in, data_t out)
+{/*
+#pragma HLS INTERFACE mode=ap_fifo port=*in
+#pragma HLS INTERFACE mode=ap_fifo port=*out
+
+
+#pragma HLS pipeline II=1 rewind
+#pragma HLS unroll factor = 2
+*/
+out[i] = in[i] * 2;
+
+}
+
+void funcD(data_t in1, data_t in2, data_t out)
+{
+	/*
+#pragma HLS INTERFACE mode=ap_fifo port=*in1
+#pragma HLS INTERFACE mode=ap_fifo port=*in2
+#pragma HLS INTERFACE mode=ap_fifo port=*out
+
+#pragma HLS pipeline II=1 rewind
+#pragma HLS unroll factor = 2
+*/
+out[i] = in1[i] + in2[i] * 2;
+
+}
